05d3cdcf671fe65f562f4376128538720cee0e13
alfangr <alfangr05@gmail.com>
2023-05-26 02:00:44 +0700

    [hotfix] update lmu values

--- app/Http/Controllers/RankingController.php
+++ app/Http/Controllers/RankingController.php
@@ -23,39 +23,39 @@ class RankingController extends Controller
     public function doRanking(Request $request)
     {
         $tfnSkala = [
-            [
+            (object)[
                 "tria" => [1, 1, 1],
                 "reci" => [1, 1, 1]
             ],
-            [
+            (object)[
                 "tria" => [0.5, 1, 1.5],
                 "reci" => [0.666, 1, 2]
             ],
-            [
+            (object)[
                 "tria" => [1, 1.5, 2],
                 "reci" => [0.5, 0.666, 1]
             ],
-            [
+            (object)[
                 "tria" => [1.5, 2, 2.5],
                 "reci" => [0.4, 0.5, 0.666]
             ],
-            [
+            (object)[
                 "tria" => [2, 2.5, 3],
                 "reci" => [0.333, 0.4, 0.5]
             ],
-            [
+            (object)[
                 "tria" => [2.5, 3, 3.5],
                 "reci" => [0.285, 0.333, 0.4]
             ],
-            [
+            (object)[
                 "tria" => [3, 3.5, 4],
                 "reci" => [0.25, 0.285, 0.333]
             ],
-            [
+            (object)[
                 "tria" => [3.5, 4, 4.5],
                 "reci" => [0.222, 0.25, 0.285]
             ],
-            [
+            (object)[
                 "tria" => [4, 4.5, 2],
                 "reci" => [0.5, 0.222, 0.25]
             ]
@@ -64,15 +64,21 @@ class RankingController extends Controller
 
         $data['criterias'] = Criteria::all();
         $data['inputCriterias'] = array_chunk($inputCriteria, count($data['criterias']));
-        // return $tfnSkala[0];
-        // return $data['inputCriterias'];
 
-        $data['conversionCriterias'] = [
-            [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
-            [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13],
-            [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 14],
-            [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 15],
-        ];
+        $lmuArray = [];
+        foreach($data['inputCriterias'] as $indexInput => $sub) {
+            foreach($sub as $indexSub => $item) {
+                if ($item == 0) {
+                    $lmuArray[] = $tfnSkala[$data['inputCriterias'][$indexInput - $indexInput][$indexSub + 1] - 1]->reci;
+                } else {
+                    $lmuArray[] = $tfnSkala[$item - 1]->tria;
+                }
+            }
+        }
+
+        // return array_chunk(array_merge(...$lmuArray), 12);
+
+        $data['conversionCriterias'] = array_chunk(array_merge(...$lmuArray), 12);
 
         return view('result', $data);
     }
